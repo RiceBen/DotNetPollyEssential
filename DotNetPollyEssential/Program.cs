8cdd0a301721f5e6cf0c5b1ef3180664ee125bbf
benjamin <benjamin750715@gmail.com>
2019-09-16 10:04:02 +0800

    Add Library project
    library that offer random exception class

--- DotNetPollyEssential/Program.cs
+++ DotNetPollyEssential/Program.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using Polly;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using UnStableService;
 
 namespace DotNetPollyEssential
 {
@@ -10,6 +10,45 @@ namespace DotNetPollyEssential
     {
         static void Main(string[] args)
         {
+            var unstableService = new MrUnstable();
+
+            ISyncPolicy redo1 = Policy.Handle<ApplicationException>()
+                                      .WaitAndRetry(2, retryDurationProvider =>
+                                      
+                                          TimeSpan.FromSeconds(Math.Pow(2, retryDurationProvider))
+                                      );
+                                      
+            ISyncPolicy redo2 = Policy.Handle<NullReferenceException>()
+                                      .Retry(2,
+                                      (exception, count) =>
+                                      {
+                                          Console.WriteLine($"NullReferenceException, exception:{exception.Message}, count:{count}");
+                                      });
+
+            ISyncPolicy redo3 = Policy.Handle<InvalidOperationException>()
+                                      .Retry(2,
+                                      (exception, count) =>
+                                      {
+                                          Console.WriteLine($"InvalidOperationException, exception:{exception.Message}, count:{count}");
+                                      });
+
+            ISyncPolicy myPolicyWrap =
+            Policy.Wrap(redo1,
+                        redo2,
+                        redo3);
+            try
+            {
+                myPolicyWrap.Execute(() => unstableService.RandomException());
+            }
+            catch (Exception)
+            {
+            }
+            finally
+            {
+                Console.WriteLine("DotNetPollyEssential Finished!");
+            }
+
+            Console.Read();
         }
     }
 }
---
+++ UnStableService/MrUnstable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace UnStableService
+{
+    /// <summary>
+    /// Mr. Unstable class
+    /// </summary>
+    public class MrUnstable
+    {
+        /// <summary>
+        /// Raise random exception
+        /// </summary>
+        /// <returns></returns>
+        public bool RandomException()
+        {
+            Random random = new Random(DateTime.UtcNow.Second);
+
+            var randomNumber = random.Next(0, 10);
+
+            if (randomNumber % 3 == 0)
+            {
+                Console.WriteLine("random application exception");
+                throw new ApplicationException("random application exception");
+            }
+            else if (randomNumber % 3 == 1)
+            {
+                Console.WriteLine("random null reference exception");
+                throw new NullReferenceException("random null reference exception");
+            }
+            else
+            {
+                Console.WriteLine("random invalid operation exception");
+                throw new InvalidOperationException("random invalid operation exception");
+            }
+        }
+    }
+}
